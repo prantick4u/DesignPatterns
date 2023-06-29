2993832c3ce1e551d191bba9f447242a0ce40107
Prantick Saha <prantick.saha@gmail.com>
2023-06-29 15:11:44 +0530

    add descrip..

--- memento/Main.java
+++ memento/Main.java
@@ -9,6 +9,16 @@ import com.designPatterns.memento.History;
 public class Main{
     public static void main(String[] args){
 
+        /*****************************************
+         * 
+         * Here the idea is the Editor(originator) will
+         * be editing the and change the states & if it 
+         * require to go back(undo) to the previous 
+         * state, then History(Caretaker) class will maintain the 
+         * State(Memento) class objects which will be restored 
+         * to the Editor.
+         * 
+         ****************************************/
         System.out.println("This is Memento\n");
         Editor editor = new Editor("first state");
         History hist = new History();
